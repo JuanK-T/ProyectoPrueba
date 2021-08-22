3a9343ab20177cbe95a10a1699997a3b08ecd088
Juank <juancatique6@gmail.com>
2021-08-21 23:10:18 -0500

    Poniendo Algunos Comentarios para dar guia

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,18 @@
-<x-app-layout>
-
-</x-app-layout>
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ColDestino| Pagina de Inicio</title>
+</head>
+<body>
+    {{-- ESTAS ETIQUETAS AGREGAN EL NAVBAR --}}
+    <x-app-layout>
+        {{-- Aqui debe esta el contenido de la Pagina  --}}
+    </x-app-layout>
+</body>
+</html>
 
 {{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
